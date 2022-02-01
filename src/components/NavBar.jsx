14df15a7f3f78222ccbed2c394e5a4ed85d364d9
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2022-02-01 19:58:02 +0100

    fix(portfolio):

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,56 +1,69 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useEffect } from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
 const Navbar = () => {
-  
-    useEffect(() => {
-        let boule= document.querySelector(".bigBoule");
-        let navButtons = document.querySelectorAll(".navButton")
-        let check=true;
-        let tailleMaxBoule =50;
-        navButtons.forEach(navButton => {
-            navButton.addEventListener("click",()=>{
-               if(check){ 
-                setTimeout(() => {
-                    boule.style.top="90%"
-                    boule.style.right="95%"
-                }, 1001);
-                   boule.animate([
-                    {top:"5%", right:"5%"},
-                    {transform:`scale(${tailleMaxBoule})`},
-                    {top:"90%", right:"95%"},
-                ],1000)
-                check=false
-               
-            }else{
-                setTimeout(() => {
-                    boule.style.right="5%"
-                    boule.style.top="5%"
-                    console.log("bonjour")
-                }, 1001);
-                boule.animate([
-                    {top:"90%", right:"95%"},
-                    {transform:`scale(${tailleMaxBoule})`},
-                    {top:"5%", right:"5%"},
-                    
-                ],1000)
-                
-                
-                check=true
-                }
-            })
-        });
+  useEffect(() => {
+    let boule = document.querySelector(".bigBoule");
+    let tailleMaxBoule = 100;
+    let check = false;
+    boule.addEventListener("click", () => {
+      if (check == false) {
+        setTimeout(() => {
+          boule.classList.toggle("scale-[100]");
+        }, 1001);
+        boule.animate([{ transform: `scale(${tailleMaxBoule})` }], 1000);
+      } else {
+        setTimeout(() => {
+          boule.classList.toggle("scale-1");
+        }, 1001);
+      }
     });
-    return (
-        <nav className='containerNav'>
-            <NavLink  to="/portfolio">
-                <div className="navButton text-center" >Portfolio</div>
-            </NavLink><br/>
-            <NavLink  to="/cv">
-                <div className="navButton text-center " >CV</div>
-            </NavLink>
-        </nav>
-    );
-}
+    // let navButtons = document.querySelectorAll(".navButton")
+    // let check=true;
+    // let tailleMaxBoule =50;
+    // navButtons.forEach(navButton => {
+    //     navButton.addEventListener("click",()=>{
+    //        if(check){
+    //         setTimeout(() => {
+    //             boule.style.top="90%"
+    //             boule.style.right="95%"
+    //         }, 1001);
+    //            boule.animate([
+    //             {top:"5%", right:"5%"},
+    //             {transform:`scale(${tailleMaxBoule})`},
+    //             {top:"90%", right:"95%"},
+    //         ],1000)
+    //         check=false
+
+    //     }else{
+    //         setTimeout(() => {
+    //             boule.style.right="5%"
+    //             boule.style.top="5%"
+    //             console.log("bonjour")
+    //         }, 1001);
+    //         boule.animate([
+    //             {top:"90%", right:"95%"},
+    //             {transform:`scale(${tailleMaxBoule})`},
+    //             {top:"5%", right:"5%"},
+
+    //         ],1000)
+
+    //         check=true
+    //         }
+    //     })
+    // });
+  });
+  return (
+    <nav className=" border w-screen">
+      <NavLink to="/portfolio">
+        <div className="z-40 relative text-white">Portfolio</div>
+      </NavLink>
+      <br />
+      <NavLink to="/cv">
+        <div className="z-40 relative text-white">CV</div>
+      </NavLink>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/components/swiperProject.jsx
+++ src/components/swiperProject.jsx
@@ -4,15 +4,46 @@ import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { motion } from "framer-motion";
 import "swiper/css";
-const RenderModalText = (props) => {
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
 
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+}
+// export const useIsSmall   = ()=>useMediaQuery("(min-width:640px)")
+
+const RenderModalText = (props) => {
+  const isSmall = useMediaQuery("(min-width:640px)");
+  const variants = isSmall
+    ? {
+        animate: { x: -200 },
+        initial: { x: 700 },
+        exit: { opacity: 0 },
+        transition:{delay:1}
+      }
+    : {
+        animate: { x: 0 },
+        initial: { x: 700 },
+        exit: { opacity: 0 },
+        transition:{delay:1}
+      };
   return (
     <motion.div
-      animate={{ x: 0 }}
-      initial={{ x: 700 }}
-      exit={{ opacity: 0 }}
-      transition={{ delay: 1 }}
-      className="bg-white rounded-lg  shadow-lg p-10 w-2/6 dark:text-white dark:bg-slate-700 self-center mr-5"
+      animate="animate"
+      initial="initial"
+      variants={variants}
+      className="bg-white rounded-lg  shadow-lg p-10 sm:w-2/6 dark:text-white dark:bg-slate-700 justify-start self-center  z-10 absolute bottom-0 sm:static"
     >
       <motion.p
         exit={{ opacity: 0 }}
@@ -32,7 +63,7 @@ const RenderModalText = (props) => {
       </motion.code>
       <br />
       <a href={props.link}>
-        <button className="rounded bg-black text-white p-3">
+        <button className="rounded  bg-gradient-to-r from-blue-500 to-cyan-500 shadow-lg shadow-cyan-500/50 text-white p-3">
           Voir le projet
         </button>
       </a>
@@ -48,18 +79,20 @@ const Swiperproject = () => {
       .then((resp) => resp.json())
       .then((data) => {
         setprojects(data);
-        console.log(data)
+        console.log(data);
       });
   }, []);
 
   const renderProject = projects.map((project) => (
-    <SwiperSlide className=" p-5 flex flex-col justify-center items-center h-1/2 ">
-      <Swiper className="shadowIn p-10 rounded-lg border-black h-2/3 border w-full" direction="horizontal">
-     
+    <SwiperSlide className=" p-5 flex flex-col sm:justify-center items-center h-1/2  ">
+      <Swiper
+        className="shadowIn p-10 rounded-lg border-black h-1/2 sm:h-2/3  w-full"
+        direction="horizontal"
+      >
         {project.imgLink.split("|").map((imgLink) => {
           return (
             <SwiperSlide className="h-full  flex justify-center items-center">
-              <img src={imgLink}  className="w-1/2"/>
+              <img src={imgLink} className="w-2/3" />
             </SwiperSlide>
           );
         })}
@@ -83,10 +116,10 @@ const Swiperproject = () => {
   );
 
   return (
-    <div className="flex w-screen">
+    <div className="flex items-center justify-center w-screen relative">
       <Swiper
         direction="vertical"
-        className="flex justify-center items-center h-screen w-2/6 swiper-logo"
+        className="flex justify-center items-center content-center h-screen   w-screen sm:w-2/6 swiper-logo"
         onSwiper={(swiper) => console.log(swiper)}
         onRealIndexChange={(swiper) => {
           setCpt(swiper.realIndex);
